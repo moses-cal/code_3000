d0a99220e6f3a7e67c3f8c4eb3ea86722d3ce6a9
Moses J Calderon <mosescal413@gmail.com>
2026-03-11 20:43:33 -0400

    adding code

--- mod06_deanonymize.py
+++ mod06_deanonymize.py
@@ -18,7 +18,25 @@ def link_records(anon_df, aux_df):
       anon_id, matched_name
     containing ONLY uniquely matched records.
     """
-    raise NotImplementedError
+    # identifyt quasi identifiers columns
+    # aka sharewd columns excluding IDs/names
+    common_cols = list(set(anon_df.columns).intersection(set(aux_df.columns)))
+    quasi_cols = [ c for c in common_cols if c not in ["anon_id", "name"] ]
+
+    # merge datasets on q identifiers
+    merged = pd.merge(anon_df, aux_df, on=quasi_cols, how="inner")
+
+    # count matches per anoymized record
+    counts = merged.groupby("anon_id").size()
+
+    # keep only uniquely matched anon_ids
+    unique_ids = counts[counts == 1].index
+    unique_matches = merged[merged["anon_id"].isin(unique_ids)]
+
+    # return the required columns
+    result = unique_matches[["anon_id", "name"]].rename(columns={"name": "matched_name"})
+
+    return result
 
 
 def deanonymization_rate(matches_df, anon_df):
@@ -26,4 +44,7 @@ def deanonymization_rate(matches_df, anon_df):
     Compute the fraction of anonymized records
     that were uniquely re-identified.
     """
-    raise NotImplementedError
+    num_matches = len(matches_df)
+    total_anon = len(anon_df)
+
+    return num_matches / total_anon
